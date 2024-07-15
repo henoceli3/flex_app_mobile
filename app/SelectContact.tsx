2a3d7d880e90bb57445e8bb6d2d40d6749e47722
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-07-15 00:41:03 +0000

    👌👌👌

--- app/SelectContact.tsx
+++ app/SelectContact.tsx
@@ -1,12 +1,134 @@
-import { AntDesign } from "@expo/vector-icons";
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Contacts from "expo-contacts";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { AppColors } from "@/constants/Colors";
 
 const SelectContact = () => {
+  const [listeContactes, setListeContactes] = useState<Contacts.Contact[]>([]);
+  const [permissionGranted, setPermissionGranted] = useState(false);
+  const [filteredContacts, setFilteredContacts] = useState<Contacts.Contact[]>(
+    []
+  );
+  const [searchText, setSearchText] = useState("");
+
+  async function getContactes() {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === "granted") {
+      setPermissionGranted(true);
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+      setListeContactes(data);
+      setFilteredContacts(data);
+    }
+  }
+  useEffect(() => {
+    getContactes();
+  }, []);
+
+  useEffect(() => {
+    const filtered = listeContactes.filter((contact) =>
+      contact.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredContacts(filtered);
+  }, [searchText, listeContactes]);
+
+  const removeCountryCode = (phoneNumber: string | undefined) => {
+    if (phoneNumber && phoneNumber.startsWith("+")) {
+      return phoneNumber.replace(/^\+\d{3}/, "");
+    }
+    return phoneNumber;
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ padding: 16 }}>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => {
+          setSearchText(text);
+        }}
+        value={searchText}
+        placeholder="Rechercher un contact"
+        autoFocus
+      />
+      <View
+        style={{
+          width: "100%",
+          marginTop: 16,
+          marginBottom: 16,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          flexDirection: "row",
+        }}
+      >
+        <AntDesign name="qrcode" size={50} color="black" />
+        <Text style={{ marginLeft: 16, fontWeight: "bold" }}>
+          Scanner pour envoyer
+        </Text>
+      </View>
+      {permissionGranted ? (
+        <>
+          <FlatList
+            data={filteredContacts}
+            renderItem={({ item }) => (
+              <View style={styles.contactItem}>
+                <View>
+                  <AntDesign name="user" size={24} color="black" />
+                </View>
+                <View>
+                  <Text style={styles.contactName}>{item.name}</Text>
+                  {item.phoneNumbers && (
+                    <Text style={styles.contactPhone}>
+                      {removeCountryCode(item.phoneNumbers[0].number)}
+                    </Text>
+                  )}
+                </View>
+              </View>
+            )}
+          />
+        </>
+      ) : (
+        <Text style={styles.permissionText}>
+          Permission non accordée pour accéder aux contacts.
+        </Text>
+      )}
     </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  button: {
+    marginBottom: 16,
+  },
+  contactItem: {
+    padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+  },
+  contactName: {
+    fontSize: 15,
+    fontWeight: "bold",
+    textTransform: "capitalize",
+  },
+  contactPhone: {
+    fontSize: 16,
+    color: "#777",
+  },
+  permissionText: {
+    fontSize: 18,
+  },
+  input: {
+    height: 40,
+    borderBottomWidth: 2,
+    borderColor: AppColors.primary,
+  },
+});
 export default SelectContact;
